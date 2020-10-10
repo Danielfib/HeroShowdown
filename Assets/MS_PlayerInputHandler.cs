1b920befcadedba551bdec9ce25914ecf5d243d8
Daniel Filgueira <dfb2@cin.ufpe.br>
2020-10-10 16:45:58 -0300

    Reorganizing project sprites and implement basic main screen options nav

---
+++ Assets/MS_PlayerInputHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
+
+public class MS_PlayerInputHandler : MonoBehaviour
+{
+    private MS_OptionsManager optionsManager;
+
+    void Start()
+    {
+        optionsManager = GameObject.FindObjectOfType<MS_OptionsManager>();
+    }
+
+    public void Navigate(CallbackContext context)
+    {
+        if (optionsManager == null || !context.performed) return;
+
+        Vector2 dir = context.ReadValue<Vector2>();
+        optionsManager.Navigate(dir);
+    }
+
+    public void Submit(CallbackContext context)
+    {
+        if (optionsManager == null || !context.performed) return;
+
+        optionsManager.Submit();
+    }
+
+    public void Cancel(CallbackContext context)
+    {
+        if (optionsManager == null || !context.performed) return;
+
+        optionsManager.Cancel();
+    }
+}
---
+++ Assets/__Scripts/UI/MS_OptionsManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MS_OptionsManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject selectorPrefab;
+    private GameObject selector;
+
+    private int selectedIndex = 0;
+
+    private void Start()
+    {
+        selector = Instantiate(selectorPrefab, transform.parent);
+        UpdateSelectorPosition();
+    }
+
+    public void Navigate(Vector2 dir)
+    {
+        if (dir.y < 0)
+            DecreaseSelectedIndex();
+        else
+            IncreaseSelectedIndex();
+
+        UpdateSelectorPosition();
+    }
+
+    public void Submit()
+    {
+
+    }
+
+    public void Cancel()
+    {
+
+    }
+
+    private void UpdateSelectorPosition()
+    {
+        selector.transform.DOMove(transform.GetChild(selectedIndex).transform.position, 0.4f);
+    }
+
+    private void IncreaseSelectedIndex()
+    {
+        if (selectedIndex + 1 <= transform.childCount - 1)
+            selectedIndex++;
+        else
+            selectedIndex = 0;
+    }
+
+    private void DecreaseSelectedIndex()
+    {
+        if (selectedIndex - 1 >= 0)
+            selectedIndex--;
+        else
+            selectedIndex = transform.childCount - 1;
+    }
+}
