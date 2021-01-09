8e9ee0cdef315350c6d6f93160e53ccf7e3e2109
Daniel Filgueira <dfb2@cin.ufpe.br>
2021-01-09 15:41:22 -0300

    Implementing Moving Platforms

---
+++ Assets/__Scripts/Networking/MovingPlatform.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+using Mirror;
+using DG.Tweening;
+using System.Collections;
+using System.Linq;
+
+public class MovingPlatform : NetworkBehaviour 
+{
+    public float speed = 5;
+    public Transform platform;
+    public Rigidbody2D rigidbody2d;
+    public Transform positionsHolder;
+
+    private Transform[] positions;
+    private Vector3 currentTargetPos;
+    private int currentTargetIndex = 0;
+
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+
+        // only simulate ball physics on server
+        rigidbody2d.simulated = true;
+
+        SetupPositions();
+        GoToNextPosition();
+    }
+
+    private void SetupPositions()
+    {
+        positions = new Transform[positionsHolder.childCount];
+        for (var i = 0; i < positions.Length; i++)
+        {
+            positions[i] = positionsHolder.GetChild(i);
+        }
+
+        currentTargetPos = positions[0].position;
+    }
+
+    [Server]
+    private void GoToNextPosition()
+    {
+        DoGoToNextPosition();
+    }
+
+    private void DoGoToNextPosition()
+    {
+        //calculate transition duration based on fixed speed
+        float duration = (currentTargetPos - platform.transform.position).magnitude / speed;
+
+        platform.DOMove(currentTargetPos, duration).OnComplete(() => 
+        {
+            currentTargetIndex = positions.GetNextIndexOfArray(currentTargetIndex);
+            currentTargetPos = positions[currentTargetIndex].position;
+            GoToNextPosition();
+        }).SetEase(Ease.Linear);
+    }
+}
---
+++ Assets/__Scripts/Utils/ArrayExtension.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ArrayExtension
+{
+    
+    public static T GetNextArrayElement<T>(this T[] array, int currentIndex)
+    {
+        int i = currentIndex;
+        if (currentIndex + 1 >= array.Length)
+        {
+            i = 0;
+        }
+        else
+        {
+            i++;
+        }
+
+        return array[i];
+    }
+
+    public static T GetPreviousArrayElement<T>(this T[] array, int currentIndex)
+    {
+        int i = currentIndex;
+        if (currentIndex + 1 >= array.Length)
+        {
+            i = 0;
+        }
+        else
+        {
+            i++;
+        }
+
+        return array[i];
+    }
+
+
+    public static int GetNextIndexOfArray(this object[] array, int currentIndex)
+    {
+        int i = currentIndex;
+        if(currentIndex + 1 >= array.Length)
+        {
+            i = 0;
+        } else
+        {
+            i++;
+        }
+
+        return i;
+    }
+
+    public static int GetPreviousIndexOfArray(this object[] array, int currentIndex)
+    {
+        int i = currentIndex;
+        if (currentIndex - 1 < 0)
+        {
+            i = array.Length - 1;
+        }
+        else
+        {
+            i--;
+        }
+
+        return i;
+    }
+}
